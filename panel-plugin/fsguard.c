14a8cf01bcac99a1886ebb87ceaf74dcd533c3a1
Mike Massonnet <mmassonnet@xfce.org>
2007-10-20 02:55:55 +0000

    * panel-plugin/fsguard.c(fsguard_new): Revert border around GtkProgressBar 	  to BORDER / 2 to fit the style of Xfce 4.4. 	* panel-plugin/fsguard.c(fsguard_set_size): Fix plugin size. 	* panel-plugin/fsguard.c(fsguard_create_options): New option dialog. 	* panel-plugin/fsguard.desktop.in.in: Use xfce4-fsguard-plugin-warning as 	  default icon. 	* *.po, *.pot: make update-po 	* de.po, fr.po: Update translation
    (Old svn revision: 3402)

--- panel-plugin/fsguard.c
+++ panel-plugin/fsguard.c
@@ -358,7 +358,7 @@ fsguard_new (XfcePanelPlugin *plugin)
       xfce_hvbox_new (orientation == GTK_ORIENTATION_HORIZONTAL ?
                       GTK_ORIENTATION_HORIZONTAL : GTK_ORIENTATION_VERTICAL,
                       FALSE, 0);
-    gtk_container_set_border_width (GTK_CONTAINER (fsguard->pb_box), 2);
+    gtk_container_set_border_width (GTK_CONTAINER (fsguard->pb_box), BORDER / 2);
 
     g_signal_connect (G_OBJECT(fsguard->btn_panel),
                       "clicked",
@@ -427,11 +427,12 @@ fsguard_set_size (XfcePanelPlugin *plugin, int size, FsGuard *fsguard)
     GtkOrientation orientation = xfce_panel_plugin_get_orientation (plugin);
     if (orientation == GTK_ORIENTATION_HORIZONTAL) {
         gtk_widget_set_size_request (GTK_WIDGET(fsguard->progress_bar), BORDER, -1);
+        gtk_widget_set_size_request (GTK_WIDGET(plugin), -1, size);
     } else {
         gtk_widget_set_size_request (GTK_WIDGET(fsguard->progress_bar), -1, BORDER);
+        gtk_widget_set_size_request (GTK_WIDGET(plugin), size, -1);
     }
 
-    gtk_widget_set_size_request (GTK_WIDGET(plugin), -1, -1);
     fsguard_refresh_icon (fsguard);
 
     return TRUE;
@@ -522,113 +523,143 @@ fsguard_dialog_response (GtkWidget *dlg, int response, FsGuard *fsguard)
 static void
 fsguard_create_options (XfcePanelPlugin *plugin, FsGuard *fsguard)
 {
-    GtkWidget *dlg;
-    GtkWidget *hbox, *vbox1, *vbox2, *spin1, *spin2;
-    GtkWidget *lab1, *lab2, *lab3, *lab4, *lab5, *lab6, *lab7, *lab8;
-    GtkWidget *ent1, *ent2, *ent3;
-    GtkWidget *check1, *check2;
-    gchar *text[] = {
-	     N_("Name"),
-	     N_("Mount point"),
-             N_("Warning limit (MB)"),
-	     N_("Urgent limit (MB)"),
-	     N_("File manager"),
-	     N_("Display size"),
-	     N_("Display progress bar"),
-	     N_("Hide button"),
-    };
     xfce_panel_plugin_block_menu (plugin);
-    
 
-    dlg = xfce_titled_dialog_new_with_buttons (_("Free Space Checker"), 
-                GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (plugin))),
-                GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_NO_SEPARATOR,
-                GTK_STOCK_CLOSE, GTK_RESPONSE_OK,
-                NULL);
-    
-    g_signal_connect (dlg, "response", G_CALLBACK (fsguard_dialog_response),
-                      fsguard);
+    /* Dialog */
+    GtkWidget *dialog =
+      xfce_titled_dialog_new_with_buttons (_("Free Space Checker"),
+        GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (plugin))),
+        GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_NO_SEPARATOR,
+        GTK_STOCK_CLOSE, GTK_RESPONSE_OK,
+        NULL);
+    gtk_window_set_icon_name (GTK_WINDOW (dialog), "xfce4-fsguard-plugin-warning");
+    gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
+    gtk_container_set_border_width (GTK_CONTAINER (GTK_DIALOG (dialog)->vbox), BORDER);
 
-    gtk_container_set_border_width (GTK_CONTAINER (dlg), 2);
-    
-    gtk_window_set_icon_name (GTK_WINDOW (dlg), "xfce4-settings");
-    
-    hbox = gtk_hbox_new (FALSE, BORDER);
-    gtk_container_set_border_width (GTK_CONTAINER (hbox), BORDER - 2);
-    
-    vbox1 = gtk_vbox_new (FALSE, BORDER);
-    vbox2 = gtk_vbox_new (FALSE, BORDER);
-
-    gtk_box_pack_start (GTK_BOX(hbox), vbox1, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(hbox), vbox2, TRUE, FALSE, 1);
-
-    lab1 = gtk_label_new (_(text[0]));
-    lab2 = gtk_label_new (_(text[1]));
-    lab3 = gtk_label_new (_(text[2]));
-    lab4 = gtk_label_new (_(text[3]));
-    lab5 = gtk_label_new (_(text[4]));
-    lab6 = gtk_label_new (NULL);
-    lab7 = gtk_label_new (NULL);
-    lab8 = gtk_label_new (NULL);
-
-    ent1 = gtk_entry_new ();
-    gtk_entry_set_max_length (GTK_ENTRY(ent1), 16);
-    gtk_entry_set_text (GTK_ENTRY(ent1), fsguard->name);
-
-    ent2 = gtk_entry_new ();
-    gtk_entry_set_max_length (GTK_ENTRY(ent2), 32);
-    gtk_entry_set_text (GTK_ENTRY(ent2), fsguard->path);
-
-    ent3 = gtk_entry_new ();
-    gtk_entry_set_max_length (GTK_ENTRY(ent3), 16);
-    gtk_entry_set_text (GTK_ENTRY(ent3), fsguard->filemanager);
-
-    spin1 = gtk_spin_button_new_with_range (0, G_MAXUINT, 100);
-    gtk_spin_button_set_value (GTK_SPIN_BUTTON(spin1), fsguard->limit_warning);
-
-    spin2 = gtk_spin_button_new_with_range (0, G_MAXUINT, 100);
-    gtk_spin_button_set_value (GTK_SPIN_BUTTON(spin2), fsguard->limit_urgent);
-
-    check1 = gtk_check_button_new_with_label (_(text[5]));
-    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(check1), fsguard->show_size);
- 
-    check2 = gtk_check_button_new_with_label (_(text[6]));
-    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(check2), fsguard->show_progress_bar);
-
-    fsguard->cb_hide_button = gtk_check_button_new_with_label (_(text[7]));
-    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(fsguard->cb_hide_button), fsguard->hide_button);
-
-    g_signal_connect (ent1, "changed", G_CALLBACK(fsguard_ent1_changed), fsguard);
-    g_signal_connect (ent2, "changed", G_CALLBACK(fsguard_ent2_changed), fsguard);
-    g_signal_connect (ent3, "changed", G_CALLBACK(fsguard_ent3_changed), fsguard);
-    g_signal_connect (spin1, "value-changed", G_CALLBACK(fsguard_spin1_changed), fsguard);
-    g_signal_connect (spin2, "value-changed", G_CALLBACK(fsguard_spin2_changed), fsguard);
-    g_signal_connect (check1, "toggled", G_CALLBACK(fsguard_check1_changed), fsguard);
-    g_signal_connect (check2, "toggled", G_CALLBACK(fsguard_check2_changed), fsguard);
-    g_signal_connect (fsguard->cb_hide_button, "toggled", G_CALLBACK(fsguard_check3_changed), fsguard);
-
-    gtk_box_pack_start (GTK_BOX(vbox1), lab1, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab2, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab5, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab3, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab4, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab6, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab7, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox1), lab8, TRUE, FALSE, 1);
-
-    gtk_box_pack_start (GTK_BOX(vbox2), ent1, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), ent2, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), ent3, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), spin1, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), spin2, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), check1, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), check2, TRUE, FALSE, 1);
-    gtk_box_pack_start (GTK_BOX(vbox2), fsguard->cb_hide_button, TRUE, FALSE, 1);
-
-    gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dlg)->vbox), hbox,
+    /* Configuration frame */
+    GtkWidget *table1 = gtk_table_new (2, 4, FALSE);
+    GtkWidget *frame1 = xfce_create_framebox_with_content (_("Configuration"), table1);
+    gtk_table_set_row_spacings (GTK_TABLE (table1), BORDER);
+    gtk_table_set_col_spacings (GTK_TABLE (table1), BORDER);
+    gtk_container_set_border_width (GTK_CONTAINER (frame1), BORDER);
+    gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), frame1,
                         TRUE, TRUE, 0);
 
-    gtk_widget_show_all (dlg);
+    GtkWidget *label1 = gtk_label_new (_("Mount point"));
+    gtk_misc_set_alignment (GTK_MISC (label1), 0, 0.5);
+    GtkWidget *entry1 = gtk_entry_new ();
+    gtk_entry_set_max_length (GTK_ENTRY (entry1), 32);
+    gtk_entry_set_text (GTK_ENTRY (entry1), fsguard->path);
+
+    GtkWidget *label2 = gtk_label_new (_("File manager"));
+    gtk_misc_set_alignment (GTK_MISC (label2), 0, 0.5);
+    GtkWidget *entry2 = gtk_entry_new ();
+    gtk_entry_set_max_length (GTK_ENTRY (entry2), 16);
+    gtk_entry_set_text (GTK_ENTRY (entry2), fsguard->filemanager);
+
+    GtkWidget *label3 = gtk_label_new (_("Warning limit (MB)"));
+    gtk_misc_set_alignment (GTK_MISC (label3), 0, 0.5);
+    GtkWidget *spin1 = gtk_spin_button_new_with_range (0, G_MAXUINT, 100);
+    gtk_spin_button_set_value (GTK_SPIN_BUTTON (spin1), fsguard->limit_warning);
+
+    GtkWidget *label4 = gtk_label_new (_("Urgent limit (MB)"));
+    gtk_misc_set_alignment (GTK_MISC (label4), 0, 0.5);
+    GtkWidget *spin2 = gtk_spin_button_new_with_range (0, G_MAXUINT, 100);
+    gtk_spin_button_set_value (GTK_SPIN_BUTTON (spin2), fsguard->limit_urgent);
+
+    gtk_table_attach_defaults (GTK_TABLE (table1), label1,
+                               0, 1, 0, 1);
+    gtk_table_attach_defaults (GTK_TABLE (table1), entry1,
+                               1, 2, 0, 1);
+    gtk_table_attach_defaults (GTK_TABLE (table1), label2,
+                               0, 1, 1, 2);
+    gtk_table_attach_defaults (GTK_TABLE (table1), entry2,
+                               1, 2, 1, 2);
+    gtk_table_attach_defaults (GTK_TABLE (table1), label3,
+                               0, 1, 2, 3);
+    gtk_table_attach_defaults (GTK_TABLE (table1), spin1,
+                               1, 2, 2, 3);
+    gtk_table_attach_defaults (GTK_TABLE (table1), label4,
+                               0, 1, 3, 4);
+    gtk_table_attach_defaults (GTK_TABLE (table1), spin2,
+                               1, 2, 3, 4);
+
+    /* Display frame */
+    GtkWidget *table2 = gtk_table_new (2, 4, FALSE);
+    GtkWidget *frame2 = xfce_create_framebox_with_content (_("User Interface"), table2);
+    gtk_table_set_row_spacings (GTK_TABLE (table2), BORDER);
+    gtk_table_set_col_spacings (GTK_TABLE (table2), BORDER);
+    gtk_container_set_border_width (GTK_CONTAINER (frame2), BORDER);
+    gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), frame2,
+                        TRUE, TRUE, 0);
+
+    GtkWidget *label5 = gtk_label_new (_("Name"));
+    gtk_misc_set_alignment (GTK_MISC (label5), 0, 0.5);
+    GtkWidget *entry3 = gtk_entry_new ();
+    gtk_entry_set_max_length (GTK_ENTRY (entry3), 16);
+    gtk_entry_set_text (GTK_ENTRY (entry3), fsguard->name);
+
+    GtkWidget *check1 = gtk_check_button_new_with_label (_("Display size"));
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check1),
+                                  fsguard->show_size);
+
+    GtkWidget *check2 = gtk_check_button_new_with_label (_("Display meter"));
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check2),
+                                  fsguard->show_progress_bar);
+
+    fsguard->cb_hide_button = gtk_check_button_new_with_label (_("Hide button"));
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (fsguard->cb_hide_button),
+                                  fsguard->hide_button);
+
+    gtk_table_attach_defaults (GTK_TABLE (table2), label5,
+                               0, 1, 0, 1);
+    gtk_table_attach_defaults (GTK_TABLE (table2), entry3,
+                               1, 2, 0, 1);
+    gtk_table_attach_defaults (GTK_TABLE (table2), check1,
+                               0, 2, 1, 2);
+    gtk_table_attach_defaults (GTK_TABLE (table2), check2,
+                               0, 2, 2, 3);
+    gtk_table_attach_defaults (GTK_TABLE (table2), fsguard->cb_hide_button,
+                               0, 2, 3, 4);
+
+    g_signal_connect (dialog,
+                      "response",
+                      G_CALLBACK (fsguard_dialog_response),
+                      fsguard);
+    g_signal_connect (entry1,
+                      "changed",
+                      G_CALLBACK (fsguard_ent1_changed),
+                      fsguard);
+    g_signal_connect (entry2,
+                      "changed",
+                      G_CALLBACK (fsguard_ent2_changed),
+                      fsguard);
+    g_signal_connect (entry3,
+                      "changed",
+                      G_CALLBACK (fsguard_ent3_changed),
+                      fsguard);
+    g_signal_connect (spin1,
+                      "value-changed",
+                      G_CALLBACK (fsguard_spin1_changed),
+                      fsguard);
+    g_signal_connect (spin2,
+                      "value-changed",
+                      G_CALLBACK (fsguard_spin2_changed),
+                      fsguard);
+    g_signal_connect (check1,
+                      "toggled",
+                      G_CALLBACK (fsguard_check1_changed),
+                      fsguard);
+    g_signal_connect (check2,
+                      "toggled",
+                      G_CALLBACK (fsguard_check2_changed),
+                      fsguard);
+    g_signal_connect (fsguard->cb_hide_button,
+                      "toggled",
+                      G_CALLBACK (fsguard_check3_changed),
+                      fsguard);
+
+    gtk_widget_show_all (dialog);
 }
 
 // }}}
