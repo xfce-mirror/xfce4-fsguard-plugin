b4e07d3782b79b9c77662a78d58865b00c40d4bb
Florian Rivoal <frivoal@xfce.org>
2010-12-15 01:08:14 +0900

    Clean up and file maintenance

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,19 @@
 #!/bin/sh
 #
-# $Id$
-#
-# Copyright (c) 2002-2005
+# Copyright (c) 2002-2010
 #         The Xfce development team. All rights reserved.
 #
-# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
-#
+
+export XDT_AUTOGEN_REQUIRED_VERSION="4.7.0"
 
 (type xdt-autogen) >/dev/null 2>&1 || {
   cat >&2 <<EOF
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
-            Please install the xfce4-dev-tools package first, it is available
-            from http://www.xfce.org/.
+            Please install the xfce4-dev-tools package first, available from
+            http://xfce.org/~benny/projects/xfce4-dev-tools/.
 EOF
   exit 1
 }
 
-# substitute revision and linguas
-linguas=`ls po/*.po 2>/dev/null | awk 'BEGIN {FS="[./]"; ORS=" "} {print $2}'`
-if [ -d .git ]; then
-    revision=$(git log --pretty=format:%h -n 1)
-else
-    revision=UNKNOWN
-fi
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.in.in" > "configure.in"
-
-exec xdt-autogen $@
-
-# vi:set ts=2 sw=2 et ai:
+xdt-autogen $@
